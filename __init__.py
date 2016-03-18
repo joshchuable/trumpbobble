38f810b90b530fdcc588cd43c4475f2f6e42a469
Joshua Chua <jc5157@truman.edu>
2016-03-17 21:55:24 -0500

    adding stripe boilerplate functions and environment vars

--- __init__.py
+++ __init__.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, render_template, make_response
 from io import StringIO
 import datetime
+import stripe
+import os
+
+stripe_keys = {
+    'secret_key': os.environ['SECRET_KEY'],
+    'publishable_key': os.environ['PUBLISHABLE_KEY']
+}
+
+stripe.api_key = stripe_keys['secret_key']
 
 app = Flask(__name__)
 
@@ -10,3 +19,23 @@ def index():
 
 if __name__ == "__main__":
 	app.run(debug=True)
+
+
+@app.route('/charge', methods=['POST'])
+def charge():
+    # Amount in cents
+    amount = 500
+
+    customer = stripe.Customer.create(
+        email='customer@example.com',
+        card=request.form['stripeToken']
+    )
+
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='usd',
+        description='Flask Charge'
+    )
+
+    return render_template('charge.html', amount=amount)
