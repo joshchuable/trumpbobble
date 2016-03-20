9a78757da825d371615c35d25f2770cded68b0c6
U-RYE-PC\RyeJ <benjaminryejones@gmail.com>
2016-03-19 20:24:28 -0500

    added checkout page

--- static/build/js/scripts.js
+++ static/build/js/scripts.js
@@ -1,14 +1,28 @@
 $(document).ready(function() {
-	$('#order-now-form').hide();
-	$('.order-now').click(function() {
-		var form = $('#order-now-form');
-		if(form.is(':visible')) {
-			form.fadeOut();
-		} else {
-			form.fadeIn();
-		}
+	var option = '';
+	for (var i=0;i<100;i++){
+	   option += '<option value="'+ i + '">' + i + '</option>';
+	}
+	$('#product-quantity-items').append(option);
+});
+
+$(document).ready(function() {
+	$('#product-quantity-items').change(function() {
+		update_subtotal();
 	});
 });
+
+function update_subtotal() {
+	var subtotal = 0;
+	$('#order-table > tbody > tr').each(function() {
+		var quantity = $(this).find('option:selected').val();
+		var price = $(this).find('.product-price > .price').val();
+		var amount = (quantity * price);
+		subtotal += amount;
+	});
+
+	$('#order-subtotal').text(subtotal);
+}
 $(document).ready(function() {
 	$('#trump-background').css('min-height', $(window).height()+'px');
 	$('#trump-background').css('min-width', $(window).width()+'px');
